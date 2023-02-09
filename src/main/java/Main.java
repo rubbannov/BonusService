e8c074a6bf4903d1a5f89b934172edc635c9e5c5
Igor Rubanov <fomagreen@yandex.ru>
2023-02-09 15:00:07 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        System.out.println(service.calculate(10_000,true));
+
+    }
+}
